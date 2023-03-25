5880bc3bc01c81ed0bb7fef6894bb6c1009ea03d
elizabethlourcey <elizabethlourcey@gmail.com>
2023-03-24 20:08:31 -0400

    added encode function

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-def encode():
+def encode(user_password):
+    encoded_password = ""
+    for char in user_password:
+        char = int(char)
+        char += 3
+        char = str(char)
+        encoded_password = encoded_password + char
+
+    print(encoded_password)
+
+def decode():
+    # hi monica! :)
     pass
 
 def main():
-    pass
-
+    # menu
+    user_password = input("Please enter numerical string to be encoded or decoded: ")
+    user_choice = input("1. Encode\n2. Decode\n")
+    if user_choice == "1":
+        encode(user_password)
+    elif user_choice == "2":
+        decode(user_password)
 
 
 if __name__ == '__main__':
